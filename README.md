0bab2188f4087716ffd22c2ddccef887a539d710
panyingyun@gmail.com <panyingyun@gmail.com>
2016-01-18 15:16:48 +0800

    aaa

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # tvdatatools
-OTT点播和榜单数据分析工具
+OTT Data Analysis Tools
+
+Usage：
+
+### 1.auto.bat 公网统计工具
+```
+tvdatatools.exe -i "testdata.csv.csv"
+```
+注意：-i 后面接跟一个csv数据文件
+
+### 2.auto_special.bat  专网统计工具
+```
+tvdatatools.exe -i "testdata.csv.csv" -t "7001A31B1"
+tvdatatools.exe -i "testdata.csv.csv" -t "8101E6OF1"
+```
+注意：-i 后面接跟一个csv数据文件 -t 后面跟着具体的TVID参数
+
+
+### 3.Running
+
+![Running Result](http://7xol9g.com1.z0.glb.clouddn.com/tvdatatools.png)
+
+### 4.Download Test Data：
+http://books.michaelapp.com/tvdatatools/testdata.csv
+
+
--- main.go
+++ main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"fmt"
-	"github.com/codegangsta/cli"
 	"os"
 	"strings"
 	"time"
+
+	"github.com/codegangsta/cli"
 )
 
 func main() {
@@ -14,23 +15,34 @@ func main() {
 	app.Usage = "Auto analysis TV's data!"
 	app.Author = "PanYingYun"
 	app.Email = "panyingyun@gmail.com/panyy@wasu.com"
-	app.Version = "1.6.0"
+	app.Version = "1.7.0"
 	app.EnableBashCompletion = true
 
 	app.Flags = []cli.Flag{
+		//全网数据
 		cli.StringFlag{Name: "input,i", Value: "", Usage: "tvdatatools -i XXX.csv"},
-		//cli.StringFlag{Name: "single,s", Value: "", Usage: "tvdatatools --single XXX.csv"},
+		//专网数据，按照TVID来进行分解
+		cli.StringFlag{Name: "tvid,t", Value: "", Usage: "tvdatatools -i XXX.csv -t 7001A31B1"},
 	}
 
 	app.Action = func(c *cli.Context) {
 		infile := c.String("input")
+		tvid := c.String("tvid")
+		fmt.Println("input = " + infile)
+		fmt.Println("tvid = " + tvid)
 
 		if len(infile) != 0 {
-			fmt.Println("input = " + infile)
-			fmt.Println(<-OTTVodData(infile))
+			if len(tvid) == 0 {
+				//全网数据统计
+				fmt.Println(<-ALLOTTVodData(infile))
+			} else {
+				//专网数据统计
+				fmt.Println(<-SpecialOTTVodData(infile, tvid))
+			}
+
 		} else {
 			fmt.Println("Input file is not exist!")
-			fmt.Println("Usage: tvdatatools --in XXX.csv")
+			fmt.Println("Usage: tvdatatools -i XXX.csv")
 		}
 
 	}
@@ -38,9 +50,9 @@ func main() {
 	app.Run(os.Args)
 }
 
-//分析和处理OTT 点播数据
+//全网OTT点播数据分析和处理
 //输入:infile 点播数据文件名
-func OTTVodData(infile string) <-chan string {
+func ALLOTTVodData(infile string) <-chan string {
 	c := make(chan string)
 	go func() {
 		//获取文件名前缀
@@ -148,6 +160,66 @@ func OTTVodData(infile string) <-chan string {
 		analysisAllTop20(videos, outfile)
 		end = time.Now()
 		fmt.Printf("\n总单个资产TOP20耗时 %v(秒)\n", end.Sub(start).Seconds())
+
+		c <- "OK, All TV Data Analysis Finish!!!"
+	}()
+	return c
+}
+
+//专网OTT点播数据分析和处理
+//输入:infile 点播数据文件名
+func SpecialOTTVodData(infile string, tvid string) <-chan string {
+	c := make(chan string)
+	go func() {
+		//获取文件名前缀
+		prefixname := strings.Replace(infile, ".csv", "", -1) + "_" + tvid + "_"
+		//从文件读取并预处理数据
+		start := time.Now()
+		videos, _ := readSpecCSV(infile, tvid)
+		end := time.Now()
+		fmt.Printf("TVID %v 数据总条数 = %v\n", tvid, len(videos))
+		fmt.Printf("TVID %v 数据读取耗时 %v(秒)\n", tvid, end.Sub(start).Seconds())
+
+		//==================点播数据统计=====================
+		outfile := prefixname + "点播.csv"
+		//总点播统计
+		start = time.Now()
+		vodt := analysisVodT(videos, outfile)
+		//各频道
+		vodp1s := analysisVodP1(videos, vodt, outfile)
+		//各频道二级栏目统计
+		vodp2ss := analysisVodP2(vodp1s, outfile)
+		end = time.Now()
+		fmt.Printf("TVID %v 点播统计耗时 %v(秒)\n", tvid, end.Sub(start).Seconds())
+
+		//====================排行数据统计===================
+		start = time.Now()
+		outfile = prefixname + "各频道三级栏目TOP20.csv"
+		analysisP1Top20P3(vodp1s, outfile)
+		end = time.Now()
+		fmt.Printf("\n TVID %v 各频道三级栏目TOP20耗时 %v(秒)\n", tvid, end.Sub(start).Seconds())
+
+		//各频道各二级栏目的TOP20单片
+		start = time.Now()
+		outfile = prefixname + "各频道各二级栏目的TOP20.csv"
+		analysisP1P2TOP20(vodp2ss, outfile)
+		end = time.Now()
+		fmt.Printf("\n TVID %v 各频道各二级栏目的TOP20耗时 %v(秒)\n", tvid, end.Sub(start).Seconds())
+
+		//各频道单个资产TOP20
+		start = time.Now()
+		outfile = prefixname + "各频道单个资产TOP20.csv"
+		analysisP1Top20(vodp1s, outfile)
+		end = time.Now()
+
+		fmt.Printf("\n TVID %v 各频道单个资产TOP20耗时 %v(秒)\n", tvid, end.Sub(start).Seconds())
+
+		//总表的资产TOP20（重点单片）
+		start = time.Now()
+		outfile = prefixname + "总单个资产TOP20(重点单片).csv"
+		analysisAllTop20(videos, outfile)
+		end = time.Now()
+		fmt.Printf("\n TVID %v 总单个资产TOP20耗时 %v(秒)\n", tvid, end.Sub(start).Seconds())
 
 		c <- "OK, All TV Data Analysis Finish!!!"
 	}()
